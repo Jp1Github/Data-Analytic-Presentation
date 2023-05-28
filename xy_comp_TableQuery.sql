e938b3aaf1506c703aa81e4b2cea15c4786385bb
Jp1Github <jpnar_99@yahoo.com>
2023-05-28 13:24:12 -0700

    Update xy_comp_TableQuery.sql

--- xy_comp_TableQuery.sql
+++ xy_comp_TableQuery.sql
@@ -24,69 +24,79 @@ ORDER BY jobTitle;
 -- Create a CTE to combine employee, employeePosition and employeesalary
 -----------------------------------------------------------------------------------
 WITH empCTE AS (
-SELECT emp.employeeID, 
-	emp.position_ID, 
-	empP.jobTitle, 
-	emp.firstName, 
+SELECT 
+    emp.employeeID, 
+    emp.position_ID, 
+    empP.jobTitle, 
+    emp.firstName, 
     emp.lastName, 
     emp.date_Of_Birth, 
     empS.salary_Per_Month
 FROM employee AS emp
-JOIN employeePosition AS empP ON emp.position_ID = empP.position_ID
-JOIN employeeSalary AS empS ON emp.employeeID = empS.employeeID )
+    JOIN employeePosition AS empP 
+	ON emp.position_ID = empP.position_ID
+    JOIN employeeSalary AS empS 
+	ON emp.employeeID = empS.employeeID )
 
-select * from empCTE;
+SELECT * FROM empCTE;
 
 
 -----------------------------------------------------------------------------------
 -- Create a VIEW to combine employee, employeePosition and employeesalary
 -----------------------------------------------------------------------------------
 CREATE VIEW empView AS 
-SELECT emp.employeeID, 
-	emp.position_ID, 
-	empP.jobTitle, 
-	emp.firstName, 
+SELECT 
+    emp.employeeID, 
+    emp.position_ID, 
+    empP.jobTitle, 
+    emp.firstName, 
     emp.lastName, 
     emp.date_Of_Birth, 
     empS.salary_Per_Month
 FROM employee AS emp
-JOIN employeePosition AS empP ON emp.position_ID = empP.position_ID
-JOIN employeeSalary AS empS ON emp.employeeID = empS.employeeID;
+JOIN employeePosition AS empP 
+    ON emp.position_ID = empP.position_ID
+JOIN employeeSalary AS empS 
+    ON emp.employeeID = empS.employeeID;
 
-select * from empView;
+SELECT * FROM empView;
 
 -- Show job title, count the total employee at each job title, sum of salary of each job title
-SELECT position_ID, jobTitle, COUNT(employeeID) AS TotalEmployees,
-	SUM(salary_Per_Month) AS TotalSalary,
-	AVG(salary_Per_Month) AS AverageSalary,
-	MIN(salary_Per_Month) AS MinSalary, 
-	MAX(salary_Per_Month) AS MaxSalary
-	FROM empView
+SELECT 
+    position_ID, jobTitle, 
+    COUNT(employeeID) AS TotalEmployees,
+    SUM(salary_Per_Month) AS TotalSalary,
+    AVG(salary_Per_Month) AS AverageSalary,
+    MIN(salary_Per_Month) AS MinSalary, 
+    MAX(salary_Per_Month) AS MaxSalary
+FROM empView
 GROUP BY jobTitle
 ORDER BY jobTitle;
 
 -- Non-aggregated values (jobTitle and Salary) in the report along with the aggregated values 
 -- partition by each job title.
-SELECT jobTitle, lastName, salary_Per_Month,
- COUNT(employeeID) OVER(PARTITION BY jobTitle) AS TotalEmployees,
- SUM(salary_Per_Month) OVER(PARTITION BY jobTitle) AS TotalSalary,
- AVG(salary_Per_Month) OVER(PARTITION BY jobTitle) AS AverageSalary,
- MIN(salary_Per_Month) OVER(PARTITION BY jobTitle) AS MinSalary,
- MAX(salary_Per_Month) OVER(PARTITION BY jobTitle) AS MaxSalary
+SELECT 
+    jobTitle, lastName, 
+    salary_Per_Month,
+    COUNT(employeeID) OVER(PARTITION BY jobTitle) AS TotalEmployees,
+    SUM(salary_Per_Month) OVER(PARTITION BY jobTitle) AS TotalSalary,
+    AVG(salary_Per_Month) OVER(PARTITION BY jobTitle) AS AverageSalary,
+    MIN(salary_Per_Month) OVER(PARTITION BY jobTitle) AS MinSalary,
+    MAX(salary_Per_Month) OVER(PARTITION BY jobTitle) AS MaxSalary
 FROM empView;
 
 
-show warnings;
+SHOW WARNINGS;
 
-select * from employeeSalary;
-select * from employee;
-select * from empSalary;
-SELECT * from employee;
+SELECT * FROM employeeSalary;
+SELECT * FROM employee;
+SELECT * FROM empSalary;
+SELECT * FROM employee;
 
-select * from employee;
-select * from employeePosition;
+SELECT * FROM employee;
+SELECT * FROM employeePosition;
 
-SHOW VARIABLES like 'sql_safe_updates';
+SHOW VARIABLES LIKE 'sql_safe_updates';
 
 
 
